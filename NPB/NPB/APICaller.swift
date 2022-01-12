d2303cdfe7dd1fe16c3f864215feea02110468b3
Paweł Brzozowski <mac@192.168.1.4>
2022-01-12 19:19:35 +0100

    retrieving data to VC using delegate-protocol pattern

--- NPB/NPB/APICaller.swift
+++ NPB/NPB/APICaller.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+protocol APIProtocol {
+    func dataRetrieved(_ retrievedData:APIData)
+}
+
 class APICaller {
-    
+    // Deleagate-Protocol pattern to retrieve data
+    var delegate: APIProtocol?
     
     // Make an API call
     func getData(from urlString: String) {
@@ -33,8 +38,15 @@ class APICaller {
                 do {
                     // Parse the JSON into the desired structure
                     let APIResponse = try decoder.decode([APIData].self, from: data!)
-                    print(APIResponse.count)
-                    print(APIResponse[0].rates.count)
+                    
+                    // Everything connected with UI should be in main thread
+                    DispatchQueue.main.async {
+                        // Temporary print info about data:
+                        print("Dowloaded \(APIResponse[0].rates.count) currencies")
+                        
+                        // Parse the returned JSON into article instances and pass it back to the view controller with the protocol and deleagte pattern
+                        self.delegate?.dataRetrieved(APIResponse[0])
+                    }
                     
                 } catch {
                     print("Sorry there was and error while decoding JSON")
--- NPB/NPB/ViewController.swift
+++ NPB/NPB/ViewController.swift
@@ -9,15 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Instance of API caller.
     let myAPICaller = APICaller()
+    
+    // Data dowloaded from API
+    var currencyArray = [currencyModel]()
+    var dataDowloadedDate: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        myAPICaller.delegate = self
         myAPICaller.getData(from: "https://api.nbp.pl/api/exchangerates/tables/A/?format=json")
     }
 
 
 }
 
+// MARK: - Article Model Protocool/ Delgate Methods
+
+// Adapt APIProtocol to retrieve data from APICaller
+extension ViewController: APIProtocol {
+    
+    // Retrieve data. From now data is no logner a list of APIData but single APIData.
+    func dataRetrieved(_ retrievedData:APIData) {
+        // Set data.
+        self.currencyArray = retrievedData.rates
+        self.dataDowloadedDate = retrievedData.effectiveDate
+        
+        print("VC: number of currencies: \(currencyArray.count)")
+        print("VC: date of dowloaded data: \(dataDowloadedDate)")
+    }
+}
