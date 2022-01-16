d4c321a6d217c699fa379136e498106630f8ec22
Paweł Brzozowski <mac@192.168.1.4>
2022-01-16 15:01:51 +0100

    allert creating frefactored

--- NPB/NPB/View/AllertCreator.swift
+++ NPB/NPB/View/AllertCreator.swift
@@ -8,9 +8,14 @@
 import Foundation
 import UIKit
 
+// This functions is responsible for creating and returning alert.
+// Allert are custom designed via specyfic parameters
 func createCustomAllert(alertTitle alertTitleString: String, alertMessage alertMess: String, actionTitle actionTitleString: String) -> UIAlertController {
+    // Create an allert
     let alert = UIAlertController(title: alertTitleString, message: alertMess, preferredStyle: .alert)
+    // Add action to the allert - this is only information kind allert
     alert.addAction(UIAlertAction(title: actionTitleString, style: .default, handler: nil))
     
+    // return created allert
     return alert
 }
